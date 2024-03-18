497ba8c6b3f0de24f548b0891923e7ec145bfd01
shubham19ellicium <shubham.kamble@ellicium.com>
2024-03-18 14:13:08 +0530

    added time zone

--- logger_log.py
+++ logger_log.py
@@ -1,27 +1,36 @@
 from datetime import datetime
+import pytz
+
+def get_local_time():
+    utc_now = datetime.now(pytz.utc)
+    local_tz = pytz.timezone('Asia/Kolkata')
+    local_time = utc_now.astimezone(local_tz)
+
+    formatted_time = local_time.strftime('%Y-%m-%d %H:%M:%S')
+    return formatted_time
 
 def log_info(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = get_local_time()
     new_log_entry = f"{timestamp}: INFO : {message}\n"
     return new_log_entry
 
 def log_error(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = get_local_time()
     new_log_entry = f"{timestamp}: ERROR : {message}\n"
     return new_log_entry
 
 def log_exception(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = get_local_time()
     new_log_entry = f"{timestamp}: EXCEPTION : {message}\n"
     return new_log_entry
 
 def log_critical(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = get_local_time()
     new_log_entry = f"{timestamp}: CRITICAL : {message}\n"
     return new_log_entry
 
 def log_warning(message):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = get_local_time()
     new_log_entry = f"{timestamp}: WARNING : {message}\n"
     return new_log_entry
 
